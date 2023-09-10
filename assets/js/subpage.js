453e37c7d85da5dd6b8e993b028bd95286072402
byanko55 <byanko55@gmail.com>
2023-09-11 01:58:59 +0900

    helper

--- assets/js/subpage.js
+++ assets/js/subpage.js
@@ -53,55 +53,43 @@ document.addEventListener('DOMContentLoaded', function(){
         hasInnerContainers: false
     });
 
-    $('.toc').addClass('toc-absolute');
-    var toc_top = $('.toc').offset().top - 165;
-    
-    $(window).scroll(function() {
-        if ($(this).scrollTop() >= toc_top) {
-            $('.toc').addClass('toc-fixed');
-            $('.toc').removeClass('toc-absolute');
-        } else {
-            $('.toc').addClass('toc-absolute');
-            $('.toc').removeClass('toc-fixed');
-        }
-    });
-
     // helper
     var helper = document.querySelector('.help_box');
+    var helperMsg = document.querySelector('.speech_bubble');
     var helperComment=0;
 
-    helper.style.display="block";
+    helper.onclick = helperTalk;
 
-    $(".help_box").click(function(){
+    function helperTalk()  {
         switch (helperComment) {
             case 0:
-                $(".speech_bubble").text("잘 봤냐 맨이야~");
+                helperMsg.innerText="잘 봤냐 맨이야~";
                 break;
             case 1:
-                $(".speech_bubble").text("쓰니의 정성을 알까?\n⬇️공감💖 누르기⬇️");
+                helperMsg.innerText="쓰니의 정성을 알까?\n⬇️공감💖 누르기⬇️";
                 break;
             case 2:
-                $(".speech_bubble").text("왕댓글/왕좋아요 주신분\n왕감사~👍");
+                helperMsg.innerText="왕댓글/왕좋아요 주신분\n왕감사~👍";
                 break;
             case 3:
-                $(".speech_bubble").text("왜 그만둬, 왜?\n이제 내용 파악 다했는데");
+                helperMsg.innerText="왜 그만둬, 왜?\n이제 내용 파악 다했는데";
                 break;
             case 4:
-                $(".speech_bubble").text("글을 스크랩해도 좋은데, 원문 전체를 대놓고 가져가는 건 안된단다!💢");
+                helperMsg.innerText="글을 스크랩해도 좋은데, 원문 전체를 대놓고 가져가는 건 안된단다!💢";
                 break;
             case 5:
-                $(".speech_bubble").text("뽈롱");
+                helperMsg.innerText="뽈롱";
                 break;
             case 6:
-                $(".speech_bubble").text("다른 글들이 궁금하면\n위의 목차를 펼쳐보렴\n🔼목차(≡) 보기🔼");
+                helperMsg.innerText="다른 글들이 궁금하면\n좌측의 목차를 살펴보렴";
                 break;
             case 7:
-                $(".speech_bubble").text("🐔 최·강·한·화 🐔");
+                helperMsg.innerText="🐔 최·강·한·화 🐔";
                 break;
             default:
-                $(".speech_bubble").text("얘, 뭐가 잘 안되니?\n⬇️질문✏️ 남기기⬇️");
+                helperMsg.innerText="얘, 뭐가 잘 안되니?\n⬇️질문✏️ 남기기⬇️";
             }
         
         helperComment = (helperComment + 1)%9;
-    });
+    }
 });
